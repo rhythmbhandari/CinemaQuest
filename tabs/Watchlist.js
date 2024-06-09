da2c19af8b2b52d4287662026c4424dc2f40bb4c
Birendra bista <birendrabista6@gmail.com>
2024-06-09 18:55:33 -0400

    #1 Start Implementation of Browse Movies

---
+++ tabs/Watchlist.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function Watchlist() {
+    return (
+        <SafeAreaView style={{ backgroundColor: 'black', flex: 1 }}>
+
+            <Text style={{ color: 'white' }}>WatchList</Text>
+
+        </SafeAreaView>
+    )
+}
