99029bfc90ebce2b40872028e6837905b679b65a
Rhythm Bhandari <rhythmbhandari@outlook.com>
2024-06-09 19:09:26 -0400

    #1 Added Tabs & API access for Browse Movies

---
+++ tabs/Home.js
@@ -0,0 +1,32 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react'
+import Dashboard from '../src/components/dashboard/Dashboard'
+import AllMovies from '../src/components/dashboard/AllMovies';
+import MovieDetails from '../src/components/dashboard/MovieDetails';
+
+
+const HomeStack = createNativeStackNavigator();
+
+const commonHeaderOptions = {
+    headerStyle: {
+        backgroundColor: 'black',
+    },
+    headerTintColor: '#fff',
+};
+
+export default function Home() {
+    return (
+
+
+
+
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Dashboard" component={Dashboard} options={{ headerShown: false }} />
+            <HomeStack.Screen name="Movies" component={AllMovies} options={commonHeaderOptions} />
+            <HomeStack.Screen name="MovieDetails" component={MovieDetails} options={commonHeaderOptions} />
+
+        </HomeStack.Navigator>
+
+
+    )
+}
---
+++ tabs/Moviecollection.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function Moviecollection() {
+    return (
+        <SafeAreaView style={{ backgroundColor: 'black', flex: 1 }}>
+
+            <Text style={{ color: 'white' }}>Movie Collection</Text>
+
+        </SafeAreaView>
+    )
+}
