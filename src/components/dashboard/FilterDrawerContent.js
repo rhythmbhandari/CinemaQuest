c52dec0211da7e9bba968c0aa9aca0c000c41820
Rhythm Bhandari <rhythmbhandari@outlook.com>
2024-07-31 16:15:58 -0400

    #15 Starts the implementation of filter movie/date

--- src/components/dashboard/FilterDrawerContent.js
+++ src/components/dashboard/FilterDrawerContent.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
-import { View, Text, Button, Pressable } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import GenreFilterOptions from '../filters/GenreFilterOptions'
-import { StyleSheet } from 'react-native'
 import { ScrollView, TextInput } from 'react-native-gesture-handler'
 import DateTimePicker from '@react-native-community/datetimepicker'
 import styles from './styles'
 
 const FilterDrawerContent = ({ closeDrawer, applyFilter, clearFilter }) => {
     const [selectedGenre, setSelectedGenre] = useState([])
-    // const [fromDate, setFromDate] = useState(new Date())
-    const [open, setOpen] = useState(false)
+    const [fromDate, setFromDate] = useState(new Date())
+    const [toDate, setToDate] = useState(new Date())
+    const [openFromDate, setOpenFromDate] = useState(false)
+    const [openToDate, setOpenToDate] = useState(false)
 
     function onGnereSelection(genreId) {
         setSelectedGenre(prevSelectedGenre => {
@@ -25,14 +26,16 @@ const FilterDrawerContent = ({ closeDrawer, applyFilter, clearFilter }) => {
     function handleApplyFilterClick() {
         const params = {
             with_genres: selectedGenre.join(','),
-            // from: fromDate,
+            'primary_release_date.gte': fromDate.toISOString().split('T')[0],
+            'primary_release_date.lte': toDate.toISOString().split('T')[0],
         }
-
         applyFilter(params)
     }
 
     function handleClearFilter() {
         setSelectedGenre([])
+        setFromDate(new Date())
+        setToDate(new Date())
         clearFilter()
     }
 
@@ -47,15 +50,7 @@ const FilterDrawerContent = ({ closeDrawer, applyFilter, clearFilter }) => {
         >
             <View>
                 <ScrollView>
-                    <Text
-                        style={{
-                            color: 'white',
-                            fontSize: 20,
-                            marginBottom: 15,
-                        }}
-                    >
-                        Genres
-                    </Text>
+                    <Text style={styles.filterHeader}>Genres</Text>
                     <GenreFilterOptions
                         selectedGenre={selectedGenre}
                         onGnereSelection={genreId => onGnereSelection(genreId)}
@@ -68,60 +63,88 @@ const FilterDrawerContent = ({ closeDrawer, applyFilter, clearFilter }) => {
                             borderBottomWidth: 1,
                         }}
                     />
-
-                    {/* <View style={styles.datePickerContainer}>
-                        <Button title="Open" onPress={() => setOpen(true)} />
+                    <Text style={styles.filterHeader}>Date</Text>
+                    <View style={styles.datePickerContainer}>
+                        <Pressable onPress={() => setOpenFromDate(true)}>
+                            <Text style={styles.datePickerLabel}>
+                                Select From Date
+                            </Text>
+                        </Pressable>
                         <TextInput
                             value={fromDate.toDateString()}
                             editable={false}
-                            placeholder="From date"
+                            style={styles.datePickerTextInput}
                         />
-                        {open && (
+                        {openFromDate && (
                             <DateTimePicker
                                 value={fromDate}
                                 onChange={(event, newDate) => {
-                                    const date = newDate.split('T')[0]
-                                    setOpen(false)
-                                    setFromDate(date)
+                                    setOpenFromDate(false)
+                                    setFromDate(newDate)
+                                    setToDate(newDate)
                                 }}
                                 mode="date"
+                                textColor="white"
+                                themeVariant="dark"
+                                style={styles.datePicker}
                             />
                         )}
-                    </View> */}
+                    </View>
+                    <View style={styles.datePickerContainer}>
+                        <Pressable onPress={() => setOpenToDate(true)}>
+                            <Text style={styles.datePickerLabel}>
+                                {' '}
+                                Select To Date
+                            </Text>
+                        </Pressable>
+                        <TextInput
+                            value={toDate.toDateString()}
+                            editable={false}
+                            placeholder="To date"
+                            style={styles.datePickerTextInput}
+                        />
+                        {openToDate && (
+                            <DateTimePicker
+                                value={toDate}
+                                onChange={(event, newDate) => {
+                                    setOpenToDate(false)
+                                    if (newDate && newDate >= fromDate) {
+                                        setToDate(newDate)
+                                    } else {
+                                        setToDate(toDate)
+                                    }
+                                }}
+                                mode="date"
+                                textColor="white"
+                                themeVariant="dark"
+                                style={styles.datePicker}
+                            />
+                        )}
+                    </View>
+                    <View
+                        style={{
+                            marginTop: 10,
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <Pressable
+                            style={styles.filterButton}
+                            onPress={() => handleApplyFilterClick()}
+                        >
+                            <Text style={{ color: 'white' }}>Apply Filter</Text>
+                        </Pressable>
+                        <Pressable
+                            style={styles.filterButton}
+                            onPress={() => handleClearFilter()}
+                        >
+                            <Text style={{ color: 'white' }}>Clear Filter</Text>
+                        </Pressable>
+                    </View>
                 </ScrollView>
-            </View>
-
-            <View
-                style={{
-                    marginTop: 10,
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                }}
-            >
-                <Pressable
-                    style={styles.filterButton}
-                    onPress={() => handleApplyFilterClick()}
-                >
-                    <Text style={{ color: 'white' }}>Apply Filter</Text>
-                </Pressable>
-                <Pressable
-                    style={styles.filterButton}
-                    onPress={() => handleClearFilter()}
-                >
-                    <Text style={{ color: 'white' }}>Clear Filter</Text>
-                </Pressable>
             </View>
         </View>
     )
 }
-
 export default FilterDrawerContent
-
-// const styles = StyleSheet.create({
-//     datePickerContainer: {
-//         flex: 1,
-//         marginTop: 10,
-//         backgroundColor: '#F5FCFF',
-//     },
-// })
